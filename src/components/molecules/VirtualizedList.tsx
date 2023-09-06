2e56d246ed1837bc10bc381e73b763128ce14787
Kauan Soares Mota <4kauanmota@gmail.com>
2023-09-06 14:55:58 -0300

    :sparkles: feat: VirtualizedList component

---
+++ src/components/molecules/VirtualizedList.tsx
@@ -0,0 +1,19 @@
+import { FlatList, StyleProp, ViewStyle } from "react-native";
+
+type VirtualizedListType = {
+  children: any;
+  style: StyleProp<ViewStyle>;
+};
+
+const VirtualizedList = ({ children, style }: VirtualizedListType) => {
+  return (
+    <FlatList
+      data={[]}
+      renderItem={null}
+      ListHeaderComponent={<>{children}</>}
+      style={style}
+    />
+  );
+};
+
+export default VirtualizedList;
