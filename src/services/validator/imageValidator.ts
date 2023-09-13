60ee5991b2c3e26fbd32dfb3d653e5740d554aee
Kauan Soares Mota <4kauanmota@gmail.com>
2023-09-13 17:41:44 -0300

    :bug: fix: Image validator fix

--- src/services/validator/imageValidator.ts
+++ src/services/validator/imageValidator.ts
@@ -1,7 +1,7 @@
-const imageValidator = (image: any) => {
+const imageValidator = (image: any = "") => {
   const imageUrl = /(http)?s?:?(\/\/[^"']*\.(?:png|jpg|jpeg|gif|png|svg))/i;
 
-  if (!image.value.match(imageUrl) && !image) {
+  if (!image.value.match(imageUrl) && image.value !== "") {
     image.errors.push("This link isn't a valid image");
   }
 };
